069e35d732ed144fb2bec586c203fd69f8bf0d20
Aécio Espinoza Miranda <49343150+AecioDev@users.noreply.github.com>
2019-12-06 21:29:07 -0400

    Commit - 06/12/2019 - 01
    Primeiro Commit do Curso

---
+++ Projeto Loja Virtual/Loja Virtual/Loja Virtual MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Loja_Virtual_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
